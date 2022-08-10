81564657833bc2de6e5136dff933e32fa742fd15
Emmanuel <emmanuel.cohen00@gmail.com>
2022-08-10 17:02:27 +0200

    add page contact

--- src/app/account/components/account/account.component.ts
+++ src/app/account/components/account/account.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.scss']
+  styleUrls: ['./account.component.scss'],
 })
 export class AccountComponent implements OnInit {
+  token!: any;
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.token = this.authService.getToken();
   }
-
 }
