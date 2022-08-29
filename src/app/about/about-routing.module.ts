c4e15f2a2031d6ce5b2f0581cc476931962d4f88
Emmanuel <emmanuel.cohen00@gmail.com>
2022-08-29 21:56:54 +0200

    add cgu and legal mentions for the website

--- src/app/about/about-routing.module.ts
+++ src/app/about/about-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
+import { CguComponent } from './components/cgu/cgu.component';
+import { LegalNoticesComponent } from './components/legal-notices/legal-notices.component';
 import { UpdateAboutComponent } from './components/update-about/update-about.component';
 
 const routes: Routes = [
   { path: '', component: AboutComponent },
   { path: 'modifier', component: UpdateAboutComponent },
+  { path: 'cgu', component: CguComponent },
+  { path: 'mentions-legales', component: LegalNoticesComponent },
 ];
 
 @NgModule({
--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -6,15 +6,27 @@ import { AboutComponent } from './components/about/about.component';
 import { UpdateAboutComponent } from './components/update-about/update-about.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { CguComponent } from './components/cgu/cgu.component';
+import { LegalNoticesComponent } from './components/legal-notices/legal-notices.component';
 
 @NgModule({
-  declarations: [AboutComponent, UpdateAboutComponent],
+  declarations: [
+    AboutComponent,
+    UpdateAboutComponent,
+    CguComponent,
+    LegalNoticesComponent,
+  ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     AboutRoutingModule,
     MatProgressSpinnerModule,
   ],
-  exports: [AboutComponent, UpdateAboutComponent],
+  exports: [
+    AboutComponent,
+    UpdateAboutComponent,
+    CguComponent,
+    LegalNoticesComponent,
+  ],
 })
 export class AboutModule {}
